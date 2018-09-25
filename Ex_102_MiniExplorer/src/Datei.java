35a8c20732906bba0b90446df1a831c68e0af552
Stefan Fürbaß <fuestc16@htl-kaindorf.ac.at>
2018-09-25 10:23:42 +0200

    created variables

--- Ex_102_MiniExplorer/src/Datei.java
+++ Ex_102_MiniExplorer/src/Datei.java
@@ -1,3 +1,6 @@
+
+import java.time.LocalDate;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,14 @@
  * @author stefan
  */
 public class Datei {
+ 
+    private String name;
+    private LocalDate change_date;
+    private double size;
+    private char read = 'R';
+    private char write = 'W';
+    private char exe = 'X';
+    private char hide = 'H';
+    
     
 }
