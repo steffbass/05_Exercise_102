96164322e24420bb4436d86d31bd98f9cae2574a
Stefan Fürbaß <fuestc16@htl-kaindorf.ac.at>
2018-09-25 10:00:46 +0200

    implemented abstract methodes

--- Ex_102_MiniExplorer/src/DateiModell.java
+++ Ex_102_MiniExplorer/src/DateiModell.java
@@ -1,3 +1,6 @@
+
+import javax.swing.AbstractListModel;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +11,16 @@
  *
  * @author stefan
  */
-public class DateiModell {
+public class DateiModell extends AbstractListModel{
+
+    @Override
+    public int getSize() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getElementAt(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
