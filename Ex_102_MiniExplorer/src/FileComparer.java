6b90db6bed243d9270aa5ba7700d79e12f956018
Stefan Fürbaß <fuestc16@htl-kaindorf.ac.at>
2018-09-25 09:56:18 +0200

    implemented Comparator

--- Ex_102_MiniExplorer/src/FileComparer.java
+++ Ex_102_MiniExplorer/src/FileComparer.java
@@ -1,3 +1,6 @@
+
+import java.util.Comparator;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +11,11 @@
  *
  * @author stefan
  */
-public class FileComparer {
-    
+public class FileComparer implements Comparator<Datei>{
+
+    @Override
+    public int compare(Datei o1, Datei o2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
