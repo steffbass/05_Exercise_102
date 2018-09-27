fda23a072b3dab14b4ac70b65a0ae5d347568b4c
Stefan Fürbaß <fuestc16@htl-kaindorf.ac.at>
2018-09-27 08:47:43 +0200

    sorting is finished

--- Ex_102_MiniExplorer/src/FileComparer.java
+++ Ex_102_MiniExplorer/src/FileComparer.java
@@ -15,7 +15,7 @@ public class FileComparer implements Comparator<Datei>{
 
     @Override
     public int compare(Datei o1, Datei o2) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       return   ((Datei) o1).getName().compareTo(((Datei) o2).getName());
     }
 
 }
