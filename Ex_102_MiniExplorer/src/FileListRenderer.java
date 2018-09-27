5c3123515a999db85c19eab0d82f929f95724d16
Stefan Fürbaß <fuestc16@htl-kaindorf.ac.at>
2018-09-27 08:10:34 +0200

    implemented renderer

--- Ex_102_MiniExplorer/src/FileListRenderer.java
+++ Ex_102_MiniExplorer/src/FileListRenderer.java
@@ -1,3 +1,10 @@
+
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +15,21 @@
  *
  * @author stefan
  */
-public class FileListRenderer {
-    
+public class FileListRenderer implements ListCellRenderer {
+
+    @Override
+    public Component getListCellRendererComponent(JList list, Object o, int i, boolean isSelected, boolean hasFocus) {
+        
+        JLabel label = new JLabel(o.toString());
+        label.setOpaque(true);
+        
+        //if( o instanceof Item ){
+          //  Item item = (Item)o;
+         //   label.setForeground(item.getColor());
+        //}
+        
+        label.setBackground( isSelected ? Color.lightGray : Color.white );
+        
+        return label;
+    }
 }
